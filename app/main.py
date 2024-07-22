93cb7e9680c84cc7fd093cf7a54392caee381150
g23ai2047 <g23ai2047@iitj.ac.in>
2024-07-22 18:29:31 +0530

    Update main.py removed comments

--- app/main.py
+++ app/main.py
@@ -17,8 +17,3 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000, host="0.0.0.0")
-
-# steps
-# 1. save all dependencies in requirements.txt file.
-# 2. create docker file in root directory of project.
-# 3. Create an image using docker build -t fastapi . command.
